2c440803aea40c60ed5a4315b0ce95373f972e08
Henna <Henna@DESKTOP-OLQP3VH>
2020-10-30 12:43:18 +0530

    Get Title of Linked Pages in separate windows

--- prac/src/test/java/com/pom/prac/pages/LinkCountPage.java
+++ prac/src/test/java/com/pom/prac/pages/LinkCountPage.java
@@ -1,12 +1,18 @@
 package com.pom.prac.pages;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.ResourceBundle.Control;
+import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import com.pom.prac.base.Utilities;
 
 public class LinkCountPage {
 
@@ -16,24 +22,38 @@ public class LinkCountPage {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
-	
+
 	@FindBy(tagName = "a")
 	List<WebElement> allLinks;
-	
+
 	@FindBy(id = "gf-BIG")
 	WebElement footerDriver;
-	
+
 	@FindBy(css = "div[id='gf-BIG'] a")
 	List<WebElement> allLinksOfFooterDriver;
-	
-	
+
 	public void gettingAllLinksCountOfEntirePage() {
 		System.out.println(allLinks.size());
 	}
-	
+
 	public void gettingAllLinksCountOfSubSet() {
 		System.out.println(footerDriver.findElements(By.tagName("a")).size());
 		System.out.println(allLinksOfFooterDriver.size());
 	}
-	
+
+	public void gettingAllLinksCountOfSubSetBySeperateTabs() {
+		for (int i = 0; i < allLinksOfFooterDriver.size(); i++) {
+			String clickLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER);
+			allLinksOfFooterDriver.get(i).sendKeys(clickLinkTab);
+		}
+
+		Set<String> windowList = driver.getWindowHandles();
+		Iterator<String> iterator = windowList.iterator();
+
+		while (iterator.hasNext()) {
+			driver.switchTo().window(iterator.next());
+			System.out.println(driver.getTitle());
+		}
+
+	}
 }
--- prac/src/test/java/com/pom/prac/tests/LinkCountTest.java
+++ prac/src/test/java/com/pom/prac/tests/LinkCountTest.java
@@ -15,12 +15,20 @@ public class LinkCountTest {
 		linkCountPage.gettingAllLinksCountOfEntirePage();
 		driver.quit();
 	}
-	
+
 	@Test
 	public void testAllLinksCountOfSubSet() {
 		WebDriver driver = Base2.setupDriver(4);
 		LinkCountPage linkCountPage = new LinkCountPage(driver);
 		linkCountPage.gettingAllLinksCountOfSubSet();
+		driver.quit();
+	}
+
+	@Test
+	public void testAllLinksCountOfSubSetWithNewTabs() {
+		WebDriver driver = Base2.setupDriver(4);
+		LinkCountPage linkCountPage = new LinkCountPage(driver);
+		linkCountPage.gettingAllLinksCountOfSubSetBySeperateTabs();
 		driver.quit();
 	}
 }
